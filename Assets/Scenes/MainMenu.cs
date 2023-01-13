f28a793f62b6e02f9a978700fc0719676bcd0bb6
SonnyFixit <fikser@tlen.pl>
2023-01-13 13:24:43 +0100

    Part 4 Sewers + part of the Main Menu Scene

---
+++ Assets/Scenes/MainMenu.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    
+
+
+
+    public void StartGame()
+    {
+    
+        //SceneManager.LoadScene();
+
+    }
+
+    public void QuitGame()
+    {
+    
+        Application.Quit();
+
+    }
+
+
+    
+    
+    
+    
+    
+}
---
+++ Assets/Scripts/DoorPanels.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorPanels : MonoBehaviour
+{
+
+    public GameObject doorLight;
+    public GameObject icon;
+    public static bool firstButton = false;
+
+     void OnTriggerStay2D(Collider2D col)
+    {
+
+
+    if (col.gameObject.tag == "Player")
+    {
+
+
+        icon.SetActive(true);
+
+
+
+
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+ 
+        doorLight.SetActive(true);
+        firstButton = true;
+
+        }
+    
+        
+    }
+    }
+
+
+
+    void OnTriggerExit2D(Collider2D col)
+    {
+        if (col.gameObject.tag == "Player")
+        {
+
+
+        icon.SetActive(false);
+        }
+    }
+
+}
