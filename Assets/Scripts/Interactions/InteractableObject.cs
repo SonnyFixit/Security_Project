824c79e035a74429a671175dc018bd9aeb84de33
SonnyFixit <fikser@tlen.pl>
2023-01-15 14:25:24 +0100

    Added outline shader graph + modified Interactable script

--- Assets/Scripts/Interactions/InteractableObject.cs
+++ Assets/Scripts/Interactions/InteractableObject.cs
@@ -8,18 +8,31 @@ public class InteractableObject : MonoBehaviour
     [SerializeField]
     public Color outlineColor;
 
+    [SerializeField]
+    private Material outlineMaterial;
+
+    [SerializeField]
+    private Material normalMaterial;
+
+    private Material currentMaterial;
+
+
     public virtual void OnMouseDown()
     {
         Debug.Log("Klikniêto na: " + name);
+        
     }
 
     public void OnMouseEnter()
     {
         Debug.Log("Mysz wesz³a na: " + name);
+        currentMaterial = GetComponent<Renderer>().material = outlineMaterial;
+
     }
 
     public void OnMouseExit()
     {
         Debug.Log("Mysz wysz³a z: " + name);
+        currentMaterial = GetComponent<Renderer>().material = normalMaterial;
     }
 }
